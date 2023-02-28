6b61bfb64fc3dda58820eaffb1bacf6840e2a09d
Lucas Souza de Oliveira <lucas.souza0673@gmail.com>
2023-02-28 19:08:01 -0300

    feat: add html da pagina quem somos

---
+++ page-quem-somos.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quem Somos - Expand Jr.</title>
+</head>
+<body>
+    <header>
+        <?php get_header(); ?>
+    </header>
+
+    <main>
+        <section class="background-overlay">
+            <div class="heading-title">
+                <h2>Conheça um pouco da nossa história</h2>
+            </div>
+        </section>
+        <section class="container-about">
+            <div class="container-about-upper-layer">
+                <div class="container-about-left-upper-layer">
+                    <div>
+                        <h2>Como tudo começou...</h2>
+                    </div>
+                    <div>
+                        <p>Fomos criados em 2016 pelo desejo de trazer uma perspectiva prática em relação ao mercado de trabalho em Relações Internacionais, por meio da vivência empresarial dentro do Movimento Empresa Júnior. Desde então, lutamos todos os dias para que nossos estudantes obtenham as melhores experiências por meio do impacto que realizamos no ecossistema do Comércio Exterior no Brasil.</p>
+                    </div>
+                </div>
+                <div class="container-about-right-upper-layer">
+                    <div>
+                        <img decoding="async" src="https://expandjr.com.br/wp-content/uploads/elementor/thumbs/Design-sem-nome-7-e1660673887936-ptczj3k8gimiytiyn97sesrqu7ziv5n19q168hi3og.png" title="Design sem nome (7)" alt="Design sem nome (7)" loading="lazy">
+                    </div>
+                </div>
+            </div>
+            <div class="container-about-lower-layer">
+               <div class="container-about-left-lower-layer">
+                    <div>
+                        <img decoding="async" width="693" height="518" src="https://expandjr.com.br/wp-content/uploads/2022/08/expandjr-com-tudo-comecou.png" class="attachment-large size-large wp-image-352" alt="" loading="lazy" srcset="https://expandjr.com.br/wp-content/uploads/2022/08/expandjr-com-tudo-comecou.png 693w, https://expandjr.com.br/wp-content/uploads/2022/08/expandjr-com-tudo-comecou-300x224.png 300w" sizes="(max-width: 693px) 100vw, 693px">
+                    </div>
+                </div>
+                <div class="container-about-right-lower-layer">
+                    <div>
+                        <div>
+                            <h2>Um pouco mais sobre nós...</h2>
+                        </div>
+                        <div>
+                            <p>Somos uma empresa júnior de consultoria internacional, gerida por alunos do curso de Relações Internacionais da Universidade Federal do Rio de Janeiro (UFRJ), eleita a melhor universidade federal do Brasil pela QS Latin American University Rankings 2022. Realizamos projetos nacionais e internacionais para pessoas físicas, startups, pequenas e médias empresas, a fim de que estas venham expandir seus seus negócios através de preços abaixo do mercado e qualidade na entrega de nossos serviços.</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <h2>Premiações</h2>
+                        </div>
+                        <div>
+                            <p>Em dezembro de 2021, no Prêmio RioJunior, a Expand recebeu três premiações referentes ao alcance de diversas metas ao longo do ano: EJ Alto Crescimento, EJ Conectada e EJ Impacto. Essas atribuições são concedidas com base em resultados nos âmbitos de faturamento, engajamento de membros, projetos de impacto e parcerias. Nesse contexto, a Expand vem desenvolvendo resultados cada vez mais positivos e demonstrando sua capacidade de oferecer soluções de alto impacto para seus clientes.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container-principles">
+            <div class="container-principles-upper-layer">
+                <div>
+                    <h2>O que nos move?</h2>
+                </div>
+            </div>
+            <div class="container-principles-lower-layer">
+                <div class="container-principles-lower-layer-card">
+                    <div>
+                        <h2>Missão</h2>
+                    </div>
+                    <div>
+                        <img decoding="async" src="https://expandjr.com.br/wp-content/uploads/elementor/thumbs/mission-pti4o5lkin5a2rja7td24qmkhghgwshqdi8v9bo9go.png" title="mission" alt="mission" loading="lazy">
+                    </div>
+                    <div>
+                        <p>Entregar soluções internacionais para nossos clientes, impactando a sociedade e desenvolvendo os alunos de RI através da experiência empreendedora.</p>
+                    </div>
+                </div>
+                <div class="container-principles-lower-layer-card">
+                    <div>
+                        <h2>Visão</h2>
+                    </div>
+                    <div>
+                        <img decoding="async" src="https://expandjr.com.br/wp-content/uploads/elementor/thumbs/Design-sem-nome-1-1-py50gyuru79vfvl66dxap12rtzw01gbrk505rxygh4.png" title="Design sem nome (1)" alt="Design sem nome (1)" loading="lazy">
+                    </div>
+                    <div>
+                        <p>Ser a melhor empresa júnior de Relações Internacionais do país e referência global em soluções internacionais de alto impacto, tendo em vista os objetivos sustentáveis da ONU.</p>
+                    </div>
+                </div>
+                <div class="container-principles-lower-layer-card">
+                    <div>
+                        <h2>Valores</h2>
+                    </div>
+                    <div>
+                        <img decoding="async" src="https://expandjr.com.br/wp-content/uploads/elementor/thumbs/target-pti4p84yaum1arzays0rgywgu97iodqq6szrxo34dk.png" title="target" alt="target" loading="lazy">
+                    </div>
+                    <div>
+                        <p>Expandir; Conexão; Propósito; Diversidade; Pertencimento; Inovação; Qualidade; Compromisso.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="container-mov-ej">
+            <div class="container-mov-ej-left-layer">
+                <div>
+                    <h2>O Movimento Empresa Junior</h2>
+                </div>
+                <div>
+                    <p>Se constitui como a principal ferramenta de educação empreendedora no ambiente acadêmico. Existe cada vez mais procura por parte de jovens universitários para participar dessa iniciativa que é regida por uma legislação própria.</p>
+                    <p>&nbsp;</p>
+                    <p>Lei 13.267/2016</p>
+                    <p>Melhorar as condições de aprendizado em nível superior; Promover o desenvolvimento econômico e social da comunidade ao mesmo tempo em que fomenta o empreendedorismo; Assessorar a implantação das soluções indicadas para os problemas diagnosticados.</p>
+                </div>
+            </div>
+            <div class="container-mov-ej-right-layer">
+                <div>
+                    <img decoding="async" width="800" height="503" src="https://expandjr.com.br/wp-content/uploads/2022/08/Captura-de-Tela-2021-09-19-às-21.20.05.png" class="attachment-large size-large wp-image-644" alt="" loading="lazy" srcset="https://expandjr.com.br/wp-content/uploads/2022/08/Captura-de-Tela-2021-09-19-às-21.20.05.png 822w, https://expandjr.com.br/wp-content/uploads/2022/08/Captura-de-Tela-2021-09-19-às-21.20.05-300x189.png 300w, https://expandjr.com.br/wp-content/uploads/2022/08/Captura-de-Tela-2021-09-19-às-21.20.05-768x483.png 768w" sizes="(max-width: 800px) 100vw, 800px">
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer>
+        <?php get_footer(); ?>
+    </footer>
+</body>
+</html>
