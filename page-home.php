9df6fcc143c3b39492bfe954a4e046b9ca56f5eb
DarkhnessXK <giovannitoledo@id.uff.br>
2023-02-28 18:17:59 -0300

    feat: metade do html da home feito

--- page-home.php
+++ page-home.php
@@ -1,7 +1,59 @@
 <?php
 get_header();
+?>
 
+<section class="section-1-home">
+    <h1>Expanda seus Horizontes</h1>
+    <h2>através das nossas soluções internacionais de alto impacto.</h2>
+    <a href="" role="button">Fale com um especialista</a>
+</section>
+<section class="section-2-services">
+    <div class="title-h2-div">
+        <h2>Prestamos Serviços Para</h2>
+    </div>
+    <div class="services-cards-div">
+        <div class="card-1">
+            <img>
+            <p>Pessoas Físicas</p>
+        </div>
+        <div class="card-2">
+            <img>
+            <p>ONG's</p>
+        </div>
+        <div class="card-3">
+            <img>
+            <p>Pessoas Jurídicas</p>
+        </div>
+    </div>
 
+</section>
 
+<section class="section-3-results">
+    <div class="title-h2-div">
+        <h2>Resultados</h2>
+    </div>
+    <div class="results-cards-div">
+        <div class="card-1">
+            <img>
+            <p>card 1</p>
+        </div>
+        <div class="card-2">
+            <img>
+            <p>card 2</p>
+        </div>
+        <div class="card-3">
+            <img>
+            <p>card 3</p>
+        </div>
+        <div class="card-4">
+            <img>
+            <p>card 4</p>
+        </div>
+    </div>
+</section>
+
+<section>
+</section>
+<?php
 get_footer();
 ?>
