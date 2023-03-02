c7a077b004b17b4a60527f9aaadbd876f10caba1
DarkhnessXK <giovannitoledo@id.uff.br>
2023-03-02 14:21:24 -0300

    fix: tentativa de carrosel

--- page-home.php
+++ page-home.php
@@ -56,26 +56,68 @@ get_header();
     <div class="title-h2-div">
         <h2>Cases de Sucesso</h2>
     </div>
-    <div class="w3-content w3-display-container" style="max-width:800px">
-  <img class="mySlides" src="img_nature_wide.jpg" style="width:100%">
-  <img class="mySlides" src="img_snow_wide.jpg" style="width:100%">
-  <img class="mySlides" src="img_mountains_wide.jpg" style="width:100%">
-  <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
-    <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
-    <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
-    <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
-  </div>
-</div>
+    <div class="w3-content w3-display-container">
+        <div class="mySlides">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, deserunt nulla. Assumenda, similique quos
+            fugiat, minima eius est incidunt tempore at nam quisquam eos eum doloribus sunt velit deleniti consequuntur?
+            <div>
+                <img>
+                <cite>
+                    <span>Sem nome 1</span>
+                </cite>
+            </div>
+        </div>
+        <div class="mySlides">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, deserunt nulla. Assumenda, similique quos
+            fugiat, minima eius est incidunt tempore at nam quisquam eos eum doloribus sunt velit deleniti consequuntur?
+            <div>
+                <img>
+                <cite>
+                    <span>Sem nome 2</span>
+                </cite>
+            </div>
+        </div>
+        <div class="mySlides">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, deserunt nulla. Assumenda, similique quos
+            fugiat, minima eius est incidunt tempore at nam quisquam eos eum doloribus sunt velit deleniti consequuntur?
+            <div>
+                <img>
+                <cite>
+                    <span>Sem nome 3</span>
+                </cite>
+            </div>
+        </div>
+        <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle">
+            <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+            <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+        </div>
+    </div>
 </section>
 
 <section class="section-5-partnerships">
     <div class="title-h2-div">
         <h2>Nossos Parceiros</h2>
     </div>
-    <div class="slide">
-        <p>lorem ipsum</p>
+    <div class="w3-content w3-display-container">
+        <div class="mySlides">
+            <img src="assets/images/1-1-150x150.png" alt="teste 1">  
+        </div>
+        <div class="mySlides">
+            <img src="" alt="teste 2"> 
+        </div>
+        <div class="mySlides">
+            <img src="" alt="teste 3"> 
+        </div>
+        <div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle">
+            <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
+            <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(1)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(2)"></span>
+            <span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(3)"></span>
+        </div>
     </div>
 </section>
 
