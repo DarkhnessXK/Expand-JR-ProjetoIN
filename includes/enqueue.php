a20bc6ab6d40652d76e1da7883b2bb3606a93e64
Fernanda Carmo <fernandadocarmo1@outlook.com>
2023-03-06 20:05:11 -0300

    :add página home-contatos

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -2,8 +2,15 @@
 
 function enqueue_expandjr_styles() {
     wp_register_style('style', get_stylesheet_uri(), [], '1.0.0', false);
-
+    wp_register_style('final-header', STYLES_DIR . '/header.css', [], '1.0.0', false);
+    wp_register_style('final-main', STYLES_DIR . '/main.css', [], '1.0.0', false);
+    
+   
+   
+   
     wp_enqueue_style('style');
+    wp_enqueue_style('final-header');
+    wp_enqueue_style('final-main');
 }
 
 ?>
