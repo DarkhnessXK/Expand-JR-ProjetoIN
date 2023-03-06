9ad84a4162585793a39aee88e4141c7d01157c06
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-03-06 19:09:52 -0300

    feat: add página quem somos

--- page-quem-somos.php
+++ page-quem-somos.php
@@ -1,7 +1,23 @@
 <?php
 get_header();
+?>
+    <main>
+        <section class='main_aside2_section'>
+            <img src="<?php the_field('imagem_sessao_principal') ?>" alt="">
+            <div class='main_aside2_text'>
+                <h2> <?php the_field('titulo_sessao_principal') ?> </h2>
+                <p> <?php the_field('texto_sessao_principal') ?> </p>
+            </div>
+        </section>
 
-
-
+        <section class='main_aside2_section aside2_section'>
+            <img src="<?php the_field('imagem_sessao_aside2') ?>" alt="">
+            <div class='main_aside2_text aside2_section_text'>
+                <h2> <?php the_field('titulo_sessao_aside2') ?> </h2>
+                <p> <?php the_field('texto_sessao_aside2') ?> </p>
+            </div>
+        </section>
+    </main>
+<?php
 get_footer();
 ?>
