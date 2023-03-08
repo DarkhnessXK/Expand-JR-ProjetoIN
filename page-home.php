1a2ab52eaa322ea2c78831932dd86095a2563a12
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-03-07 21:00:37 -0300

    fix: corrigindo bug do whatsapp

--- page-home.php
+++ page-home.php
@@ -200,6 +200,7 @@ get_header();
         </div>
     </div>
 </section>
+<?php $cel = get_option('zap_cadastro_telefone'); ?>
 <a class="whatsapp-link" href="https://web.whatsapp.com/send?phone=<?php echo $cel; ?>" target_blank="_blank">
   <img src="<?php echo IMAGES_DIR . '/icon-whatsapp-48.png'; ?>" alt="Ícone do Whatsapp">
 </a>
--- page-quem-somos.php
+++ page-quem-somos.php
@@ -18,6 +18,7 @@ get_header();
             </div>
         </section>
     </main>
+    <?php $cel = get_option('zap_cadastro_telefone'); ?>
     <a class="whatsapp-link" href="https://web.whatsapp.com/send?phone=<?php echo $cel; ?>" target_blank="_blank">
         <img src="<?php echo IMAGES_DIR . '/icon-whatsapp-48.png'; ?>" alt="Ícone do Whatsapp">
     </a>
