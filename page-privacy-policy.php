1e49b8c45d882d2d34da0cbe9bb285c8fa20984a
Luiz Felyppe <felyppe.nunes1@gmail.com>
2023-03-09 23:25:48 -0300

    fix: funcionalidade abrir outra página whatsapp

---
+++ page-privacy-policy.php
@@ -0,0 +1,11 @@
+<?php
+get_header();
+?>
+<main>
+    <a href="<?php echo get_field('termos_de_uso'); ?>" class="">Termos de Uso</a>
+    <a href="<?php echo get_field('politicas_de_privacidade'); ?>" class="">Políticas de Privacidade</a>
+</main>
+
+<?php
+get_footer();
+?>
