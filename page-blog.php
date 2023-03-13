147e0ce0f14a1a8588edc2ddba68bf5ef1b7a43d
DarkhnessXK <giovannitoledo@id.uff.br>
2023-03-13 11:14:38 -0300

    feat: estilo e html para as noticias do blog

--- page-blog.php
+++ page-blog.php
@@ -7,7 +7,36 @@ get_header();
 </section>
 
 <section class="section-2-blog">
+  <div class="blog-width-div">
+    <?php
+    $objeto = new WP_Query(array('category_name' => 'post-blog'));
 
+    if ($objeto->have_posts()):
+
+      while ($objeto->have_posts()):
+        $objeto->the_post();
+
+        ?>
+
+        <div class="blog-post-div">
+          <img src="<?php the_field('image-blog'); ?>" class="blog-image">
+          <div class="content-blog">
+            <h2 class="news-title">
+              <?php the_title(); ?>
+            </h2>
+            <h6 class="blog-subtitle-category">
+              <?php the_category(); ?>
+            </h6>
+            <?php the_content(); ?>
+            <a type="button" href="" class="read-more-blog">Leia Mais</a>
+          </div>
+        </div>
+      <?php endwhile;
+      wp_reset_postdata();
+      ?>
+
+    <?php endif; ?>
+  </div>
 </section>
 
 <?php $cel = get_option('zap_cadastro_telefone'); ?>
