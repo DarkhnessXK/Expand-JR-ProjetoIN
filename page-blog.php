2433ba94ee8ee6d71ff0e7e713d4cae4d1c14bc4
DarkhnessXK <giovannitoledo@id.uff.br>
2023-03-13 10:25:57 -0300

    feat: criando seção 2 do blog

--- page-blog.php
+++ page-blog.php
@@ -1,15 +1,19 @@
-<?php 
+<?php
 get_header();
 ?>
 <section class="section-1-blog">
-    <h1 class="section-1-blog-title">NOSSO BLOG</h1>
-    <p class="section-1-blog-subtitle">Nos conectamos para conectar o mundo</p>
+  <h1 class="section-1-blog-title">NOSSO BLOG</h1>
+  <p class="section-1-blog-subtitle">Nos conectamos para conectar o mundo</p>
+</section>
+
+<section class="section-2-blog">
+
 </section>
 
 <?php $cel = get_option('zap_cadastro_telefone'); ?>
 <a class="whatsapp-link" href="https://web.whatsapp.com/send?phone=<?php echo $cel; ?>" target="_blank">
   <img src="<?php echo IMAGES_DIR . '/icon-whatsapp-48.png'; ?>" alt="Ícone do Whatsapp">
 </a>
-<?php 
+<?php
 get_footer();
 ?>
